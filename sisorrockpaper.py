36f6330b1797a72fea03e347e602a0346a9d9f1e
Brunavizzotto <bruna.vizzotto0@gmail.com>
2026-03-02 16:46:22 -0300

    rock paper scissors

---
+++ sisorrockpaper.py
@@ -0,0 +1,20 @@
+import random
+
+user_choice = input("Choose rock, paper or scissors: ")
+
+choices = ["rock", "scissors", "paper"]
+
+computer_choice = random.choice(choices)
+
+if computer_choice == user_choice:
+    print("It's a tie!")
+elif computer_choice == "rock" and user_choice == "scissors":
+    print("Computer wins!")
+elif computer_choice == "scissors" and user_choice == "paper":
+    print("Computer wins!")
+elif computer_choice == "paper" and user_choice == "rock":
+    print("Computer wins!")
+else:
+    print("You win!")
+
+print("I choose", computer_choice, "and you choose", user_choice)
